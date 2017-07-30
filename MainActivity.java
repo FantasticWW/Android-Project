b8f0ee529c6a83a18ecb78961d8b14d5be03d4ba
rBrilliant <499953902@qq.com>
2017-07-30 19:05:28 +0800

    custom dialog in bottom

--- MainActivity.java
+++ MainActivity.java
@@ -1,7 +1,14 @@
 package com.example.com.customdialog;
 
+import android.app.Dialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +17,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void openDialog(View view) {
+        Dialog dialog = new Dialog(this, R.style.MyDialog);
+        //加载布局
+        View dialogView = LayoutInflater.from(this).inflate(R.layout.custom_dialog,null);
+
+        dialog.setContentView(dialogView);
+
+        //设置对话框显示的位置
+        Window dialogWindow = dialog.getWindow();
+        dialogWindow.setGravity(Gravity.BOTTOM);
+        WindowManager.LayoutParams lp = dialogWindow.getAttributes();
+        lp.y = 10;
+        dialogWindow.setAttributes(lp);
+
+        dialog.show();
+
+    }
+
 }
